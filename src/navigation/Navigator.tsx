ca75e6ac2cc98afa33239fdf6bb634e64ed23f57
alexelizaga <alexelizaga@icloud.com>
2022-04-20 08:03:47 +0200

    285

--- src/navigation/Navigator.tsx
+++ src/navigation/Navigator.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import {PermissionsScreen} from '../pages/PermissionsScreen';
 import {MapScreen} from '../pages/MapScreen';
+import {PermisionsContext} from '../context/PermissionsContext';
+import {LoadingScreen} from '../pages/LoadingScreen';
 
 const Stack = createStackNavigator();
 
 export const Navigator = () => {
+  const {permissions} = useContext(PermisionsContext);
+
+  if (permissions.locationStatus === 'unavailable') {
+    return <LoadingScreen />;
+  }
+
   return (
     <Stack.Navigator
       initialRouteName="PermissionScreen"
@@ -16,8 +24,11 @@ export const Navigator = () => {
           backgroundColor: 'white',
         },
       }}>
-      <Stack.Screen name="MapScreen" component={MapScreen} />
-      <Stack.Screen name="PermissionScreen" component={PermissionsScreen} />
+      {permissions.locationStatus === 'granted' ? (
+        <Stack.Screen name="MapScreen" component={MapScreen} />
+      ) : (
+        <Stack.Screen name="PermissionScreen" component={PermissionsScreen} />
+      )}
     </Stack.Navigator>
   );
 };
